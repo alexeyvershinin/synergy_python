218ac8019dee8c65cbd7291a39e3a580b24f7929
alexeyvershinin <115144824+alexeyvershinin@users.noreply.github.com>
2023-10-03 17:42:51 +0300

    Modified the code according to the task

--- Lesson #8. Lists/task_2.py
+++ Lesson #8. Lists/task_2.py
@@ -1,12 +1,15 @@
 def main():
     N = int(input('Введите количество чисел: '))
     numbers = list(map(int, input('Введите числа через пробел: ').split()))
-    modified_numbers = [0] * N
 
-    for i in range(N):
-        modified_numbers[i] = numbers[(N - 1 - i) % N]
+    last_element = numbers[-1]
 
-    print(*modified_numbers)
+    for i in range(N - 1, 0, -1):
+        numbers[i] = numbers[i - 1]
+
+    numbers[0] = last_element
+
+    print(*numbers)
 
 if __name__ == "__main__":
     main()
